7bed559a7a7d86b4d4d82a5abb307f0bb8fd99d9
jai <jai@Jai-LM>
2018-09-01 11:28:48 +0930

    Fixed light field renderer

--- Fragment.fs
+++ Fragment.fs
@@ -15,6 +15,22 @@ uniform vec2 rotation;
 out vec4 color;
 
 vec3 rotate(vec3 ray, vec2 angle) {
+  //rotate y
+  float y = -sin(angle.y)*ray.z;
+  float z = cos(angle.y)*ray.z;
+  ray.y = y;
+  ray.z = z;
+
+  //rotate x
+  float x = -sin(angle.x)*ray.z;
+  z = cos(angle.x)*ray.z;
+  ray.x = x;
+  ray.z = z;
+
+  return ray;
+}
+
+vec3 rotate2(vec3 ray, vec2 angle) {
 	//rotate x
 	float y = cos(angle.x)*ray.y - sin(angle.x)*ray.z;
 	float z = cos(angle.x)*ray.z + sin(angle.x)*ray.y;
@@ -33,7 +49,7 @@ vec3 rotate(vec3 ray, vec2 angle) {
 void main(void) {
 	//vec3 ray = rotate(vec3(0,0,-1), vec2(texcoord.x*M_PI, texcoord.y*M_PI/2));
 	vec3 ray = vec3(texcoord.x, texcoord.y, -1);
-  ray = rotate (ray, rotation*M_PI/180);
+  ray = rotate2(ray, rotation*M_PI/180);
 
   float a = ray.x*ray.x + ray.y*ray.y + ray.z*ray.z;
   float b = 2*ray.x*position.x + 2*ray.y*position.y + 2*ray.z*position.z;
@@ -48,12 +64,27 @@ void main(void) {
   }
   vec3 point = ray*t + position;
   vec2 pointPolar = vec2(atan(point.x,-point.z), asin(point.y));
-  vec2 intermediateAngle = positionPolar.xy - pointPolar;
-  vec3 intermediatePoint = vec3(sin(intermediateAngle.x)*positionPolar.z,
-    sin(intermediateAngle.y)*positionPolar.z, -cos(intermediateAngle.x)*positionPolar.z + 1);
-  float len = sqrt(intermediatePoint.x*intermediatePoint.x + intermediatePoint.y*intermediatePoint.y + intermediatePoint.z*intermediatePoint.z);
-  vec2 finalAngle = vec2(atan(intermediatePoint.x,-intermediatePoint.z), asin(intermediatePoint.y/len));
-  //color = texture2D(lightField, round((texcoord/2+0.5)*(imageSize+1))/(imageSize+1));
-  vec2 spherePoint = vec2(pointPolar.x/M_PI/2+0.5,pointPolar.y/M_PI+0.5);
-  color = texture2D(lightField, floor(spherePoint*(imageSize+1))/(imageSize+1) - finalAngle/M_PI/imageSize); //FIXME
+
+	vec2 leng;
+	vec2 positionDistance;
+	vec3 cr = cross(point, vec3(-point.z,0,point.x));
+	t = (cr.x*point.x - cr.x*position.x + cr.y*point.y - cr.y*position.y + cr.z*point.z - cr.z*position.z)/(cr.x*cr.x + cr.y*cr.y + cr.z*cr.z);
+	vec3 proj = position + t*cr;
+	leng.x = sqrt((proj.x-point.x)*(proj.x-point.x) + (proj.y-point.y)*(proj.y-point.y) + (proj.z-point.z)*(proj.z-point.z));
+	positionDistance.x = sqrt(proj.x*proj.x + proj.y*proj.y + proj.z*proj.z);
+
+	t = (-point.z*point.x + point.z*position.x + point.x*point.z - point.x*position.z)/(point.z*point.z + point.x*point.x);
+	proj = position + t*vec3(-point.z, 0, point.x);
+	leng.y = sqrt((proj.x-point.x)*(proj.x-point.x) + (proj.y-point.y)*(proj.y-point.y) + (proj.z-point.z)*(proj.z-point.z));
+	positionDistance.y = sqrt(proj.x*proj.x + proj.y*proj.y + proj.z*proj.z);
+
+	vec2 finalAngle = acos((1 + leng*leng - positionDistance*positionDistance)/(2*leng));
+	if (positionPolar.y < pointPolar.y) {
+		finalAngle.y *= -1;
+	}
+	if (positionPolar.x < pointPolar.x == abs(positionPolar.x - pointPolar.x) < M_PI) {
+		finalAngle.x *= -1;
+	}
+	vec2 spherePoint = vec2(pointPolar.x/M_PI/2+0.5,pointPolar.y/M_PI+0.5);
+	color = texture2D(lightField, (floor(spherePoint*imageSize)+0.5)/imageSize - finalAngle/M_PI/imageSize);
 }
